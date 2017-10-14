3fe5d9106c0e3568f38a95d9febe32e155a2745a
sletteon <misi.g126@gmail.com>
2017-10-14 16:42:04 +0200

    Kilépés, eredeti kommentek törlése

--- client.py
+++ client.py
@@ -4,28 +4,31 @@ import select
 import sys
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if len(sys.argv) != 2:
-	print "Így használd: <szkript> IP-cím"
+if len(sys.argv) != 3:
+	print "Így használd: <szkript> IP-cím, Port"
 	exit()
 IP_address = str(sys.argv[1])
-#Port = int(sys.argv[2])
-Port = 5000
+Port = int(sys.argv[2])
 server.connect((IP_address, Port))
 
 while True:
-	sockets_list = [sys.stdin, server]
-	
-	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
-	
-	for socks in read_sockets:
-		if socks == server:
-			message = socks.recv(2048)
-			print message
-		else:
-			sys.stdout.write("<Te>")
-			message = sys.stdin.readline()
-			server.send(message)
-			sys.stdout.write(message)
-			sys.stdout.flush()
+	try:
+		sockets_list = [sys.stdin, server]
+		
+		read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+		
+		for socks in read_sockets:
+			if socks == server:
+				message = socks.recv(2048)
+				print message
+			else:
+				sys.stdout.write("<Te>")
+				message = sys.stdin.readline()
+				server.send(message)
+				sys.stdout.write(message)
+				sys.stdout.flush()
+	except KeyboardInterrupt:
+		server.send('AUT: Kiléptem.')
+		exit()
 server.close()
 
